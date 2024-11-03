a7e2883a4df4b2b00bf921d360fb91909b421bf2
BalaVamsi1 <balavamsilanke1@gmail.com>
2024-11-03 17:52:22 +0530

    initial commit

---
+++ src/main/java/com/test/demo/DevopsApplication.java
@@ -0,0 +1,40 @@
+package com.test.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DevopsApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello() {
+      return String.format("<h1>Hello this is my first spring boot project<h1>");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class DevopsApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DevopsApplication.class, args);
+//	}
+//
+//}
